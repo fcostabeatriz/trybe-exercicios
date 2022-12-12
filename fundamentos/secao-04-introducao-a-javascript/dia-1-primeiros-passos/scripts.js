ef3d5dbbbea81b55cf8326653bad68a19357f739
Beatriz Costa <fcostabeatriz@gmail.com'clear'add pastas e readme':qw:wq>
2022-12-12 17:43:43 -0300

    add javascript

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
@@ -0,0 +1,14 @@
+const myName = "Beatriz";
+let birthCity = "Recife"; 
+
+let birthYear = 1995; 
+
+console.log(myName); 
+console.log(birthCity);
+console.log(birthYear); 
+
+birthYear = 2030;
+console.log(birthYear);;
+
+birthCity = "Paris";
+console.log(birthCity);
